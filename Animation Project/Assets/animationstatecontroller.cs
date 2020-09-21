01c8909573740e70612ece675e5ff7447d7c9c36
rmontgomery1 <shadmont2940@yahoo.com>
2020-09-21 13:38:58 -0500

    animation
    character can idle, walk, and run.

---
+++ Animation Project/Assets/animationstatecontroller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationstatecontroller : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+        isWalkingHash = Animator.StringToHash("isWalking");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isRunning = animator.GetBool("isRunning");
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+         if (isWalking && !forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+        if (!isRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool("isRunning", true);
+        }
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
